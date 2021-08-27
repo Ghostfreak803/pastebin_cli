4e1a74b046e8f4cec0a338c408dd7e1f98e68d16
Rishi Verma <rishi.verma803@gmail.com>
2021-08-27 19:56:53 +0530

    add the ability to paste strings from commandline

--- pasteapi.py
+++ pasteapi.py
@@ -2,24 +2,29 @@ import requests
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('mode', choices=['show', 'paste'])
+parser.add_argument('mode', choices=['show', 'file', 'paste'])
 parser.add_argument('input')
 args = parser.parse_args()
 
 user_input = args.input
 dev_key = 'f6ade61387a928553d5ad87814931857'
 user_key = '273a606bd17c017c932123cf12be1ad0'
+api_url = 'https://pastebin.com/api/api_post.php'
 
-if args.mode == 'show':
+if args.mode == 'paste':
+    api_data = {'api_dev_key': dev_key, 'api_option': 'paste', 'api_paste_code': user_input}
+    req = requests.post(api_url, api_data)
+    print(req.text.replace('pastebin.com', 'pastebin.com/raw'))
+
+
+elif args.mode == 'show':
     req = requests.get(user_input)
     print(req.text)
 
 else:
-    api_url = 'https://pastebin.com/api/api_post.php'
-
     with open(args.input, 'r', encoding = 'utf-8') as file:
         data = file.read()
 
     api_data = {'api_dev_key': dev_key, 'api_option': 'paste', 'api_paste_code': data}
     req = requests.post(api_url, api_data)
-    print(req.text)
+    print(req.text.replace('pastebin.com', 'pastebin.com/raw'))
