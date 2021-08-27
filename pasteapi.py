956ed049a0a54d216a5350546d42babaf0195409
Rishi Verma <rishi.verma803@gmail.com>
2021-08-27 20:35:44 +0530

    added regex to parse user url

--- pasteapi.py
+++ pasteapi.py
@@ -1,3 +1,4 @@
+import re
 import requests
 import argparse
 
@@ -11,18 +12,22 @@ dev_key = 'f6ade61387a928553d5ad87814931857'
 user_key = '273a606bd17c017c932123cf12be1ad0'
 api_url = 'https://pastebin.com/api/api_post.php'
 
-if args.mode == 'paste':
+# Paste from command line:
+if args.mode == 'paste': 
     api_data = {'api_dev_key': dev_key, 'api_option': 'paste', 'api_paste_code': user_input}
     req = requests.post(api_url, api_data)
     print(req.text.replace('pastebin.com', 'pastebin.com/raw'))
 
-
+# Open a paste url:
 elif args.mode == 'show':
-    if 'raw' not in user_input:
+    if re.search(r'^(https?:\/\/)?pastebin.com\/(\w{8})', user_input) == None:
+        print("Not a pastebin url")
+    elif 'raw' not in user_input:
         user_input = user_input.replace('pastebin.com', 'pastebin.com/raw')
-    req = requests.get(user_input)
-    print(req.text)
+        req = requests.get(user_input)
+        print(req.text)
 
+# Upload content of a file
 else:
     with open(args.input, 'r', encoding = 'utf-8') as file:
         data = file.read()
